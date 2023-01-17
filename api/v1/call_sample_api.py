c6e94825c4fafa7a93ea2ccbe40f37fa657bd900
Kelvin Hung <kelvinhung@astri.org>
2023-01-17 06:50:18 +0000

    revised to support file upload

--- api/v1/call_sample_api.py
+++ api/v1/call_sample_api.py
@@ -6,12 +6,23 @@ from api.v1.main import *
 from flask import request, jsonify, Response
 
 def post():
+
+    body = request
+
     try:
         startTime = datetime.now()
         logging.info("The sample API server is called at: " + str(startTime))
 
+        ##to receive uploaded file
+        print("body:", body)
+
+        if request.method == 'POST':
+            f = request.files['file']
+            f.save((f.filename))
+            print("file:", f)
+
         ##proceed your tasks
-        ref_datetime = test_func(startTime)
+        #ref_datetime = test_func(startTime)
 
         endTime = datetime.now()
         logging.info("Completion time at: " + str(endTime))
--- app.py
+++ app.py
@@ -7,8 +7,13 @@ from flask_cors import CORS
 
 logging.basicConfig(level = logging.INFO)
 
+UPLOAD_FOLDER = "/python_api_sample_1/dataset/"
+
 app = connexion.FlaskApp(__name__, specification_dir = "openapi/")
 app.add_api("api.yaml", resolver = RestyResolver('api.v1'))
+app.app.config['UPLOAD_EXTENSIONS'] = ['.xlsx', '.csv']
+app.app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 CORS(app.app)
 
 app.run(port = int (os.environ["PORT"]), server = 'gevent')
