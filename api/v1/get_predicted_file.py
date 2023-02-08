c481b2b46fde4b750684d2ed6d52c691faa76b66
Kelvin Hung <kelvinhung@astri.org>
2023-02-08 09:25:30 +0000

    add download report endpoint 08Feb

--- api/v1/get_predicted_file.py
+++ api/v1/get_predicted_file.py
@@ -1,20 +1,26 @@
 import json
 from datetime import datetime
 import logging
-import flask
 import os
-import time
-import pathlib
 from api.v1.main import *
-from flask import request, jsonify, Response
+from flask import Response, send_file
 
 def post():
 
     try:
         logging.info("The sample API server is called at: " + str(datetime.now()))
+        
+        os.chdir(os.getcwd() + '/dataset')
 
-        response = Response(json.dumps({'Success':'true'}), 200, mimetype = 'application/json')
-        return response
+        ##response = Response(json.dumps({'Success':'true'}), 200, mimetype = 'application/json')
+        
+        ##response = send_from_directory(os.getcwd() , "Date_Fruit_Datasets_test", as_attachment=True)
+
+        return send_file(os.getcwd() + "/Date_Fruit_Datasets_test.csv", 
+        mimetype='file/csv',
+        as_attachment = True)
+        
+        ##return send_from_directory(os.getcwd() , "Date_Fruit_Datasets_test.csv", as_attachment = True)
 
     except Exception as e:
         response = Response(json.dumps(
